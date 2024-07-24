999ca7422963db87d2df8747128d0154cbe5bd96
nour-ferhi <ferhinour@dopacode.com>
2024-07-24 22:56:03 +0100

    Second Commit

--- main.py
+++ main.py
@@ -1,41 +1,49 @@
 import random
 
-choices= ["rock","paper","scissor"]
-computer= random.choice(choices)
-user = input("User: ").lower()
-while user not in choices:
+while True:
+
+    choices= ["rock","paper","scissor"]
+    computer= random.choice(choices)
     user = input("User: ").lower()
+    while user not in choices:
+        user = input("User: ").lower()
 
-if computer == user:
-    print("user=",user)
-    print("Computer=",computer)
-    print("computer == You")
-elif computer == "rock":
-    if user == "paper":
-        print("user=", user)
-        print("Computer=", computer)
-        print("You Win :)")
-    else:
-        print("user=", user)
-        print("Computer=", computer)
-        print("You Lose :(")
+    if computer == user:
+        print("user=",user)
+        print("Computer=",computer)
+        print("computer == You")
+    elif computer == "rock":
+        if user == "paper":
+            print("user=", user)
+            print("Computer=", computer)
+            print("You Win :)")
+        else:
+            print("user=", user)
+            print("Computer=", computer)
+            print("You Lose :(")
 
-elif computer == "paper":
-    if user == "scissor":
-        print("user=", user)
-        print("Computer=", computer)
-        print("You Win :)")
-    else:
-        print("user=", user)
-        print("Computer=", computer)
-        print("You Lose :(")
+    elif computer == "paper":
+        if user == "scissor":
+            print("user=", user)
+            print("Computer=", computer)
+            print("You Win :)")
+        else:
+            print("user=", user)
+            print("Computer=", computer)
+            print("You Lose :(")
 
-elif computer == "scissor":
-    if user == "paper":
-        print("user=", user)
-        print("Computer=", computer)
-        print("You Lose :(")
-    else:
-        print("user=", user)
-        print("Computer=", computer)
-        print("You Win :)")
+    elif computer == "scissor":
+        if user == "paper":
+            print("user=", user)
+            print("Computer=", computer)
+            print("You Lose :(")
+        else:
+            print("user=", user)
+            print("Computer=", computer)
+            print("You Win :)")
+
+    play_again= input("play again (yes/no): ").lower()
+    if play_again != "yes":
+        break
+
+print("Bye! Se You Soon...")
